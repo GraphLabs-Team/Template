2e51fcaadb7a516d11ed21fedd2459b076d5f240
AnutaWest <ane4karpova2003@gmail.com>
2024-05-16 19:54:04 +0300

    [refactor: update my module]

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import {Template} from './components/Template';
-import { ModuleExample } from './components/ModuleExample/module';
+import { ModuleExample } from './components/ShortestPathModule/module';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<ModuleExample/>, document.getElementById('root'));
